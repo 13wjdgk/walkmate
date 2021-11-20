1e0d3915f11dcc4846560a7950729b2bc38aab35
13wjdgk <13wjdgk@naver.com>
2021-11-21 02:31:44 +0900

    nav바 mypage 수정

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 const session=async()=>{
-    const account = await axios.get("../php/account/main.php");
+    const account = await axios.get("../php/account/checkSession.php");
     console.log(account.data[0].real_id);
     if(account.data[0].real_id){
     $('.menu' ).append('<a class="menu_a" href="./mypage.html">마이페이지</a>/<a class="menu_a" href="../php/account/logout.php">로그아웃</a>' );}
